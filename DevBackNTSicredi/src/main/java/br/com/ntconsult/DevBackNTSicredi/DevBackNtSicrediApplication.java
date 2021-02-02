9370b8f7d784e3d01d0ba0c41163afa022d2c460
thallyssonklein <thallyssonklein@gmail.com>
2021-02-02 19:41:11 -0300

    Initial Structure

---
+++ DevBackNTSicredi/src/main/java/br/com/ntconsult/DevBackNTSicredi/DevBackNtSicrediApplication.java
@@ -0,0 +1,13 @@
+package br.com.ntconsult.DevBackNTSicredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBackNtSicrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBackNtSicrediApplication.class, args);
+	}
+
+}
