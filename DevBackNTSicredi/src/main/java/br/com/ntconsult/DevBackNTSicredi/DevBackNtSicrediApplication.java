93ce1fd2545e941d718a91016c47e963a0e1e662
thallyssonklein <thallyssonklein@gmail.com>
2021-02-04 16:08:49 -0300

    Internal server error

--- DevBackNTSicredi/src/main/java/br/com/ntconsult/DevBackNTSicredi/DevBackNtSicrediApplication.java
+++ DevBackNTSicredi/src/main/java/br/com/ntconsult/DevBackNTSicredi/DevBackNtSicrediApplication.java
@@ -2,6 +2,7 @@ package br.com.ntconsult.DevBackNTSicredi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
