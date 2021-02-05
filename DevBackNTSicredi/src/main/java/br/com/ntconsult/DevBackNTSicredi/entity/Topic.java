fe062e4d6af5871fc9127334b29384ee42c96e7f
thallyssonklein <thallyssonklein@gmail.com>
2021-02-05 17:21:03 -0300

    Post topic is working

--- DevBackNTSicredi/src/main/java/br/com/ntconsult/DevBackNTSicredi/entity/Topic.java
+++ DevBackNTSicredi/src/main/java/br/com/ntconsult/DevBackNTSicredi/entity/Topic.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class Topic {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "ID")
     private long id;
 
