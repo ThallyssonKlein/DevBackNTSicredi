5c2d3f5b101144b7702f3f47cbc1276b05015112
thallyssonklein <thallyssonklein@gmail.com>
2021-02-05 17:56:49 -0300

    Swagger-ui fixed + README.md

--- DevBackNTSicredi/src/main/java/br/com/ntconsult/DevBackNTSicredi/DevBackNtSicrediApplication.java
+++ DevBackNTSicredi/src/main/java/br/com/ntconsult/DevBackNTSicredi/DevBackNtSicrediApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "br.com.ntconsult.DevBackNTSicredi")
+@EnableSwagger2
 public class DevBackNtSicrediApplication {
 
 	public static void main(String[] args) {
---
+++ README.md
@@ -0,0 +1,11 @@
+# DevBackNTSicredi
+## Para rodar
+
+- [ ] Instale o [docker](https://docs.docker.com/engine/install/) e o [docker-compose](https://docs.docker.com/compose/install/) caso não tenha instalado 
+- [ ] Rode o comando abaixo na raiz do repositório
+```
+sudo docker-compose up -d
+```
+## Como usar
+
+Para aprender a usar a API você pode acessar a [doc](http://localhost:8080/swagger-ui.html) assim que os containers estiverem rodando.
